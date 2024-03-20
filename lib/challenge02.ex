9074f00bb0698c67e754a12c62cd585fd84e2eb4
techuila <axlcuyugan05@gmail.com>
2024-03-20 22:59:05 +0800

    feat: initial commit for day 2

---
+++ lib/challenge02.ex
@@ -0,0 +1,18 @@
+defmodule Day2Challenge do
+  def merge_and_sum(map1, map2) do
+    # Your implementation goes here
+  end
+
+  # Helper functions can be defined here
+end
+
+# Example usage
+defmodule ExampleUsageDay2 do
+  def run do
+    IO.inspect Day2Challenge.merge_and_sum(%{"a" => 10, "b" => 20}, %{"a" => 5, "c" => 30})
+    # Add more examples as needed
+  end
+end
+
+# Running the examples
+ExampleUsageDay2.run()
